78c2a6ff1317b0580b127d8226ce38812ba3db17
Kirk Morris <kirk@saricden.com>
2014-12-06 22:28:06 -0800

    Adds dummy actions.

--- backend/includes/actions.php
+++ backend/includes/actions.php
@@ -3,5 +3,14 @@
 include('logic.php');
 
 class Actions extends Logic {
-
+    
+    // Returns array of (int) area id mapped to the (string) area name for a given lat & long
+    public function getArea(lat, long) {
+        return array(1 => 'The Annex');
+    }
+    
+    // Returns (int) for a given (int) areaID
+    public function getCrimeRating(areaID) {
+        return 1;
+    }
 }
