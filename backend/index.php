d49f65c7f0b5b751df08ddbe90e28edc4dce7b7d
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-12-07 00:01:42 -0500

    Update index.php

--- backend/index.php
+++ backend/index.php
@@ -1,3 +1,18 @@
 <?php
+$action = $_REQUEST['action'];
+if(empty($action)) {
+  // action is empty. return error.
+  http_response_code(400);
+  exit;
+}
 
+include('includes/backend.php');
+
+$backend = new Backend;
+$backend->setAction($action);
+
+$response = $backend->getResponse(true); // get response as json.
+
+header('Content-Type: application/json');
+echo $response;
 ?>
