e007ad5c015cbe787838f743c3a1824d4baff513
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-12-07 04:20:27 -0500

    implementing multiple functions. crime rating logic still pending implementation

--- backend/includes/actions.php
+++ backend/includes/actions.php
@@ -5,12 +5,40 @@ include('logic.php');
 class Actions extends Logic {
     
     // Returns array of (int) area id mapped to the (string) area name for a given lat & long
-    public function getArea(lat, long) {
-        return array(1 => 'The Annex');
+    public function getArea($id) {
+        $query = "SELECT * FROM areas WHERE id = ?";
+        $data = array($id);
+        $area = $this->queryDatabase($query, $data);
+        return array(
+            "id": $id,
+            "name": $area['name'],
+            "cityid": $area['cityid'],
+        );
+    }
+    
+    public function getCity($id) {
+        $query = "SELECT * from cities WHERE id = ?";
+        $data = array($id);
+        $city = $this->queryDatabase($query, $data);
+        return array(
+            "id": $id,
+            "name": $city['name'],
+        );
     }
     
     // Returns (int) for a given (int) areaID
-    public function getCrimeRating(areaID) {
-        return 1;
+    public function getCrimeRating($areaID, $filter=null) {
+        $crimes = null;
+        if($filter !== null) {
+            $crimes = $this->getCrimes($areaID, $filter);
+        }
+        else {
+            $crimes = $this->getCrimes($areaID);
+        }
+        // calculate the amount of unique years.
+        $years = array();
+        foreach($crimes as $c) {
+            
+        }
     }
 }
