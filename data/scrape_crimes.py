3ea087229d2ddb7fffc0bc48b0054d9770a5b900
Giles Edkins <edkins@gmail.com>
2014-12-07 02:33:57 -0500

    crime database scraper

---
+++ data/scrape_crimes.py
@@ -0,0 +1,19 @@
+import csv
+
+def process_row(row, firstrow):
+    assert(len(row) == len(firstrow))
+    result = {}
+    for i in range(len(row)):
+        result[firstrow[i]] = row[i]
+    print(result)
+
+def process_csv(filename):
+    with open(filename + '.csv', newline='') as f:
+        reader = csv.reader(f)
+        firstrow = None
+        for row in reader:
+            if firstrow == None:
+                firstrow = row
+            elif row != []:
+                process_row(row, firstrow)
+process_csv('assault')
