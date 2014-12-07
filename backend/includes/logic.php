dd2b32bb88ca5f6d7076cd657a93f1b7a606ee30
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-12-07 01:22:36 -0500

    Update logic.php

--- backend/includes/logic.php
+++ backend/includes/logic.php
@@ -2,11 +2,17 @@
 
 class Logic {
 
+    public $db;
+
     protected $config;
     
     public function __construct() {
         $config = $this->getConfig('../config/config.php');
         $this->config = $config;
+        if($config['testing'] !== true) {
+            // run the database connection if not testing.
+            $this->connectToDatabase();
+        }
     }
     
     public function getConfig($config_file = 'config.php') {
@@ -19,20 +25,17 @@ class Logic {
     }
 
     public function connectToDatabase() {
+        // Connect to the database using config values using PDO.
         $database = $this->config['database'];
         $db = new PDO('mysql:host=localhost;dbname=testdb;charset=utf8', $database['user'], $database[''];
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        // Save the database instance to the global db variable.
         $this->db = $db;
     }
 
     public function getData() {
-        if($this->config['testing'] === true) {
-            // in testing mode.
-        }
-        else {
-            // product mode.
-        }
+        
     }
 
 }
