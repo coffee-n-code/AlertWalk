b6cd46680f168bbee0a3ba7c390311ee869521f0
Giles Edkins <edkins@gmail.com>
2014-12-07 07:04:22 -0500

    more useful crime scraper

--- data/scraper/scrape_crimes.py
+++ data/scraper/scrape_crimes.py
@@ -2,6 +2,7 @@ import simplejson
 import csv
 
 stuff = []
+outfile = open('../crimes.csv', 'w')
 
 def process_row(row, firstrow, crime_name):
     assert(len(row) == len(firstrow))
@@ -9,7 +10,14 @@ def process_row(row, firstrow, crime_name):
     for i in range(len(row)):
         if firstrow[i] != "":
             result[firstrow[i]] = row[i]
-    result['crime_name'] = crime_name
+    #result['crime_name'] = crime_name
+    for k in result:
+        if k >= '2004' and k <= '2011':
+            amount = result[k]
+            year = k
+            neighbourhood_id = result['Number']
+            neighbourhood = result['Name']
+            outfile.write(neighbourhood_id + ",\"" + neighbourhood + '\",\"' + crime_name + "\"," + year + "," + amount + "\n")
     stuff.append(result)
 
 def process_csv(filename, crime_name):
@@ -21,14 +29,14 @@ def process_csv(filename, crime_name):
                 firstrow = row
             elif row != []:
                 process_row(row, firstrow, crime_name)
-process_csv('assault','Assault')
-process_csv('breakandenter','Breaking and entering')
-process_csv('drug-charges','Drug charges')
-process_csv('murder','Murder')
-process_csv('robbery','Robbery')
-process_csv('sexualassault','Sexual assault')
-process_csv('stolen-cars','Stolen cars')
-process_csv('theft','Theft')
+process_csv('assault','assault')
+process_csv('breakandenter','break and enter')
+process_csv('drug-charges','drug charges')
+process_csv('murder','murder')
+process_csv('robbery','robbery')
+process_csv('sexualassault','sexual assault')
+process_csv('stolen-cars','stolen vehicle')
+process_csv('theft','theft $5000+')
 
 outfile = open('../json/crimes.json','w')
 outfile.write(simplejson.dumps(stuff))
