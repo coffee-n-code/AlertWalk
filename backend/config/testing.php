39503f78ae39d217beef8780e1acbb05fb82a450
Kirk Morris <kirk@saricden.com>
2014-12-06 23:32:54 -0800

    Simplifies test data array a bit.

--- backend/config/testing.php
+++ backend/config/testing.php
@@ -1,27 +1,25 @@
 <?php
 return array(
-    "testing_data" => array(
-        "crimes" => array(
-            array(
-                "cityid" => 1,
-                "areaid" => 1,
-                "type" => "assault",
-                "amount" => 20,
+    "crimes" => array(
+        array(
+            "cityid" => 1,
+            "areaid" => 1,
+            "type" => "assault",
+            "amount" => 20,
+        ),
+    ),
+    "areas" => array(
+        array(
+            "name" => "Example Area",
+            "coords" => array( // lat => lon
+                10 => 20,
+                30 => 20,
             ),
         ),
-        "areas" => array(
-            array(
-                "name" => "Example Area",
-                "coords" => array( // lat => lon
-                    10 => 20,
-                    30 => 20,
-                ),
-            ),
-        ),
-        "cities" => array(
-            array(
-                "name" => "Example City"
-            ),
+    ),
+    "cities" => array(
+        array(
+            "name" => "Example City"
         ),
     ),
 );
