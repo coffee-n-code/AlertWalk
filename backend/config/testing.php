f5e82b2ef002f778ba05a6df40a85b8bfbbfce1c
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-12-07 00:30:42 -0500

    Create testing.php

---
+++ backend/config/testing.php
@@ -0,0 +1,27 @@
+<?php
+return array(
+    "testing_data" => array(
+        "crimes" => array(
+            array(
+                "cityid" => 1,
+                "areaid" => 1,
+                "type" => "assault",
+                "amount" => 20,
+            ),
+        ),
+        "areas" => array(
+            array(
+                "name" => "Example Area",
+                "coords" => json_encode(array(
+                    10 => 20,
+                    30 => 20,
+                )),
+            ),
+        ),
+        "cities" => array(
+            array(
+                "name" => "Example City"
+            ),
+        ),
+    ),
+);
