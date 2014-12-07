e3506457ac98220fb1f9c18f149c4bb1cf3b0ecf
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-12-07 05:24:41 -0500

    updating information to PDO

--- backend/includes/logic.php
+++ backend/includes/logic.php
@@ -34,7 +34,7 @@ class Logic {
     public function connectToDatabase() {
         // Connect to the database using config values using PDO.
         $database = $this->config['database'];
-        $db = new PDO('mysql:host=localhost;dbname=testdb;charset=utf8', $database['user'], $database[''];
+        $db = new PDO('mysql:host='.$database['host'].';port='.$database['port'].';dbname='.$database['name'].';charset=utf8', $database['user'], $database['pass']);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
         // Save the database instance to the global db variable.
