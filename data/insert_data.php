441abf30ebb42e406d83f0c8dbda4bd50d253afe
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-12-07 07:12:39 -0500

    Update insert_data.php

--- data/insert_data.php
+++ data/insert_data.php
@@ -1,5 +1,5 @@
 <?php
-$json_file = "";
+$json_file = "json/crimes.json";
 
 $json = file_get_contents($json_file);
 $data = json_decode($json, true);
@@ -16,3 +16,10 @@ $database = array(
 $db = new PDO('mysql:host='.$database['host'].';port='.$database['port'].';dbname='.$database['name'].';charset=utf8', $database['user'], $database['pass']);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+$number = 1;
+foreach($data as $d) {
+  print "Inserting Query #".$number;
+  $db->query('INSERT INTO crimes(id, name, type, year, amount) VALUES('.$d[0].', '.$d[1].', '.$d[2].', '.$d[3].', '.$d[4].')');
+  $number++;
+}
