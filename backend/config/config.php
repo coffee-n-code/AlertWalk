288888572c07c7d302e4863d5ef820f3aa59fed3
Matthew Gross <MatthewGross@users.noreply.github.com>
2014-12-07 04:37:38 -0500

    adding importance values for different types of crime.

--- backend/config/config.php
+++ backend/config/config.php
@@ -8,4 +8,14 @@ return array(
         "host" => "localhost",
         "port" => 3306, // 3306 by default.
     ),
+    "importance" => array(
+        "assault" => 0.15,
+        "sexual_assault" => 0.175,
+        "break_and_enter" => 0.225,
+        "robbery" => 0.175,
+        "drug_charges" => 0.15,
+        "stolen_vehicle" => 0.2,
+        "theft" => 0.2125,
+        "murder" => 1, // 1 is base.
+    ),
 );
